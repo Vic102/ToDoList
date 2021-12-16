dfdbe9112c37d19fb12d63b091fe9540e967685f
Victor Riddlestone Aguilar <victorriddlestoneaguilar@gmail.com>
2021-12-16 09:41:11 +0100

    Varios cambios

--- todolist/javaJs.js
+++ todolist/javaJs.js
@@ -14,15 +14,15 @@ function setFormMessage(message) {
   const messageBox = document.querySelector('.list');
   messageBox.innerHTML = "+ " + message;
   messageBox.style.fontWeight = 'bold';
-  messageBox.style.display = 'block';
+  //messageBox.style.display = 'block';
 }
 
 let lengthThings = 0;
 function keepData(event) {
   const thingToDo = document.querySelector('#text-input').value;
+  things[lengthThings] = thingToDo;
+  setFormMessage(things[lengthThings]);
   lengthThings++;
-  for (lengthThings; )
-  setFormMessage(thingToDo);
   console.log(thingToDo);
   return stopExecution(event);
 }
