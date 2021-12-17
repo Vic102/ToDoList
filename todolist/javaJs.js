6243969a4d13fbabf17fbce580f0529e7171fb49
Victor Riddlestone Aguilar <victorriddlestoneaguilar@gmail.com>
2021-12-17 12:44:20 +0100

    Terminado.

--- todolist/javaJs.js
+++ todolist/javaJs.js
@@ -4,12 +4,28 @@ function stopExecution(e) {
   return;
 }
 
+function buttonChange(event) {
+  let content = event.target;
+   if (content.innerHTML=='To Do'){
+    content.innerHTML='In Progress';
+    content.style.backgroundColor = 'yellow';
+  }
+  else if (content.innerHTML=='In Progress'){
+    content.innerHTML='Done';
+    content.style.backgroundColor = 'red';
+  }
+  else {
+    content.innerHTML='To Do';
+    content.style.backgroundColor = 'lightgreen';
+  }
+}
+
 function setFormMessage(message) {
   const messageBox = document.querySelector('.list');
   if (message == "") {
     return;
   }
-  messageBox.innerHTML += "<li>" + message + '</li>';
+  messageBox.innerHTML += "<li> <button class='common-style' id='myBtn' onclick='buttonChange(event)'>To Do</button> " + " " + message + '</li>';
 }
 
 function keepData(event) {
